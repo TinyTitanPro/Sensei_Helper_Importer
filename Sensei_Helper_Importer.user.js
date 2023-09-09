4a39a29bbb1252439c760d93d465b6d7bc296697
TinyTitanPro <703497097@qq.com>
2023-09-09 17:00:07 +0800

    Update Sensei_Helper_Importer.user.js
    Update CSS Style

--- Sensei_Helper_Importer.user.js
+++ Sensei_Helper_Importer.user.js
@@ -12,14 +12,34 @@
 
 (function() {
     'use strict';
-    let importbox = document.createElement('textarea');
+
+    let importbox = document.createElement('input');
     let importbtn = document.createElement('button');
+    let btntext = document.createElement('div');
+    let btnclick = document.createElement('span');
+
 
     let front = "{\"equipmentsRequirementStore\":{\"requirementByPieces\":[";
     let end = "],\"requirementMode\":\"ByPiece\"},\"gameInfoStore\":{\"gameServer\":\"Global\"}}";
 
     importbox.placeholder = '在此粘贴导入代码';
-    importbtn.textContent = '导入素材需求';
+    btntext.innerText = '导入素材需求';
+
+    importbtn.className = 'css-a89121';
+    importbox.style.position = 'relative';
+    importbox.style.left = '340px';
+    importbox.style.right = '340px';
+
+    importbox.className = 'css-1v4ccyo';
+    importbtn.style.position = 'relative';
+    importbtn.style.left = '350px';
+    importbtn.style.right = '350px';
+
+    btntext.className = ' revert-wiz-transform';
+    btnclick.className = 'MuiTouchRipple-root css-w0pj6f';
+
+
+    //importbtn.textContent = '导入素材需求';
     importbtn.addEventListener('click',() => {
         let importcode = front+importbox.value+end;
         window.localStorage.setItem("SenseiHelperStore",importcode);
@@ -30,5 +50,7 @@
 
     document.querySelector("#__next > div.MuiGrid2-root.MuiGrid2-container.MuiGrid2-direction-xs-row.css-6mqsus > div > div > div > div > div:nth-child(1) > div.BuiLinedText_textRowContainer__gFUcJ").appendChild(importbox);
     document.querySelector("#__next > div.MuiGrid2-root.MuiGrid2-container.MuiGrid2-direction-xs-row.css-6mqsus > div > div > div > div > div:nth-child(1) > div.BuiLinedText_textRowContainer__gFUcJ").appendChild(importbtn);
+    importbtn.appendChild(btntext);
+    importbtn.appendChild(btnclick);
 
 })();
